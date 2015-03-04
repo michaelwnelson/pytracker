5e514557308f4acc1b6adb37246d5520a2035fcc
Michael Nelson <michaelnelson07@gmail.com>
2015-03-04 14:02:52 -0600

    GetStory: Fix retrieving a single story
    We'll re-use _ApiQueryStories to inherit the requested_by fields that we
    want and FromJson expects.

--- pytracker.py
+++ pytracker.py
@@ -116,8 +116,9 @@ class Tracker(object):
     return lst
 
   def GetStory(self, story_id):
-    story_json = self._Api('stories/%d' % story_id, 'GET')
-    return Story.FromJson(json.loads(story_json))
+    data = self._ApiQueryStories('id:%d' % story_id)
+    story = json.loads(data)
+    return Story.FromJson(story[0])
 
   def AddComment(self, story_id, comment):
     comment = json.dumps({'text':comment})
